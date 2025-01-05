1a04cb40740e1bdd8f5f5e60fc7bc707f29fed3c
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2025-01-05 23:16:51 +0200

    Update JoinNewsLetter.jsx

--- src/ui/JoinNewsLetter.jsx
+++ src/ui/JoinNewsLetter.jsx
@@ -1,58 +1,58 @@
 import { MdOutlineMailOutline } from "react-icons/md";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import TextRevealAnimation from "./TextRevealAnimation";
 
 function JoinNewsLetter() {
   return (
-    // <AnimatePresence initial={window.innerWidth >= 680}>
-    <div className="relative mt-20 h-[360px] w-full overflow-hidden bg-[#F2F4F6] max-sm:mt-10">
-      <motion.img
-        src="/category3.png"
-        className="absolute left-[-140px] top-[-18px] h-[490px] w-[480px] mix-blend-darken max-lg:w-[380px] max-md:hidden"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.6, delay: 0.1 }}
-        viewport={{ once: true }}
-      />
-      <motion.img
-        src="/chair.png"
-        className="absolute bottom-[-180px] right-[-210px] h-[500px] w-[640px] mix-blend-darken max-lg:w-[460px] max-md:hidden"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.6, delay: 0.1 }}
-        viewport={{ once: true }}
-      />
-      <div className="flex h-full flex-col items-center justify-center text-black-shade-1">
-        <div className="mb-2 w-fit text-[40px] font-medium leading-[44px] tracking-[-0.4px] max-sm:text-[34px]">
-          <TextRevealAnimation color={"#F2F4F6"}>
-            <p className="text-nowrap">Join Our Newsletter</p>
-          </TextRevealAnimation>
-        </div>
-        <div className="font-inter text-lg max-sm:text-sm">
-          <TextRevealAnimation color={"#F2F4F6"}>
-            <p className="text-nowrap">
-              Sign up for deals, new products and promotions
-            </p>
-          </TextRevealAnimation>
-        </div>
-        <motion.div
-          className="mt-8 flex h-[48.8px] w-[488px] items-center overflow-hidden border-b-[.5px] border-[#6c727580] font-inter font-medium text-black-shade-4 max-lg:w-[400px] max-sm:mx-auto max-sm:w-[311px]"
-          initial={{ height: 0 }}
-          whileInView={{ height: "48.8px" }}
-          transition={{ duration: 0.5, delay: 0.4 }}
+    <AnimatePresence initial={window.innerWidth >= 680}>
+      <div className="relative mt-20 h-[360px] w-full overflow-hidden bg-[#F2F4F6] max-sm:mt-10">
+        <motion.img
+          src="/category3.png"
+          className="absolute left-[-140px] top-[-18px] h-[490px] w-[480px] mix-blend-darken max-lg:w-[380px] max-md:hidden"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.1 }}
           viewport={{ once: true }}
-        >
-          <MdOutlineMailOutline className="mr-2 h-6 w-6 text-black" />
-          <input
-            type="text"
-            className="w-full bg-transparent text-black-shade-4 placeholder:font-inter placeholder:font-medium placeholder:text-black-shade-4 focus:outline-none"
-            placeholder="Email address"
-          />
-          <button className="ml-auto h-fit">Signup</button>
-        </motion.div>
+        />
+        <motion.img
+          src="/chair.png"
+          className="absolute bottom-[-180px] right-[-210px] h-[500px] w-[640px] mix-blend-darken max-lg:w-[460px] max-md:hidden"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.1 }}
+          viewport={{ once: true }}
+        />
+        <div className="flex h-full flex-col items-center justify-center text-black-shade-1">
+          <div className="mb-2 w-fit text-[40px] font-medium leading-[44px] tracking-[-0.4px] max-sm:text-[34px]">
+            <TextRevealAnimation color={"#F2F4F6"}>
+              <p className="text-nowrap">Join Our Newsletter</p>
+            </TextRevealAnimation>
+          </div>
+          <div className="font-inter text-lg max-sm:text-sm">
+            <TextRevealAnimation color={"#F2F4F6"}>
+              <p className="text-nowrap">
+                Sign up for deals, new products and promotions
+              </p>
+            </TextRevealAnimation>
+          </div>
+          <motion.div
+            className="mt-8 flex h-[48.8px] w-[488px] items-center overflow-hidden border-b-[.5px] border-[#6c727580] font-inter font-medium text-black-shade-4 max-lg:w-[400px] max-sm:mx-auto max-sm:w-[311px]"
+            initial={{ height: 0 }}
+            whileInView={{ height: "48.8px" }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            viewport={{ once: true }}
+          >
+            <MdOutlineMailOutline className="mr-2 h-6 w-6 text-black" />
+            <input
+              type="text"
+              className="w-full bg-transparent text-black-shade-4 placeholder:font-inter placeholder:font-medium placeholder:text-black-shade-4 focus:outline-none"
+              placeholder="Email address"
+            />
+            <button className="ml-auto h-fit">Signup</button>
+          </motion.div>
+        </div>
       </div>
-    </div>
-    // </AnimatePresence>
+    </AnimatePresence>
   );
 }
 
