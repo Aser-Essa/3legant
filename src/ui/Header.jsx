7403e3449e3065e3f84c9cfe88e782bbae684601
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2025-01-08 02:01:29 +0200

    Update Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,13 +5,15 @@ import Logo from "./Logo";
 
 function Header() {
   return (
-    <div className="flex h-[60px] items-center justify-between px-[160px] max-lg:px-20 max-md:px-10 max-sm:px-8">
-      <div className="flex items-center gap-1">
-        <FlyMenu />
-        <Logo className={"max-sm:text-base"} />
+    <div className="h-[60px]">
+      <div className="fixed top-10 z-[100000] flex h-[60px] w-full items-center justify-between bg-white px-[160px] max-lg:px-20 max-md:px-10 max-sm:px-8">
+        <div className="flex items-center gap-1">
+          <FlyMenu />
+          <Logo className={"max-sm:text-base"} />
+        </div>
+        <NavLinks />
+        <Icons />
       </div>
-      <NavLinks />
-      <Icons />
     </div>
   );
 }
--- src/ui/Offer.jsx
+++ src/ui/Offer.jsx
@@ -13,33 +13,35 @@ function Offer() {
   }
 
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          className="relative flex h-10 items-center justify-center gap-3 bg-white-shade-2 font-inter text-sm font-semibold"
-          initial={{ translateY: "-100%" }}
-          animate={{ translateY: "0" }}
-          exit={{ translateY: "-100%" }}
-          transition={{ duration: 0.35 }}
-          key="box"
-        >
-          <CiDiscount1 className="h-6 w-6" />
-          <p className="">30% off storewide — Limited time! </p>
-          <ArrowLink
-            className={
-              "border-blue-custom font-medium text-blue-custom max-sm:hidden"
-            }
-            to={"/shop"}
+    <div className="h-10">
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="fixed top-0 z-[100000] flex h-10 w-full items-center justify-center gap-3 bg-white-shade-2 font-inter text-sm font-semibold"
+            initial={{ translateY: "-100%" }}
+            animate={{ translateY: "0" }}
+            exit={{ translateY: "-100%" }}
+            transition={{ duration: 0.35 }}
+            key="box"
           >
-            Shop Now
-          </ArrowLink>
-          <IoCloseOutline
-            className="absolute right-4 h-5 w-5 cursor-pointer max-sm:relative max-sm:ml-[22px]"
-            onClick={handleClick}
-          />
-        </motion.div>
-      )}
-    </AnimatePresence>
+            <CiDiscount1 className="h-6 w-6" />
+            <p className="">30% off storewide — Limited time! </p>
+            <ArrowLink
+              className={
+                "border-blue-custom font-medium text-blue-custom max-sm:hidden"
+              }
+              to={"/shop"}
+            >
+              Shop Now
+            </ArrowLink>
+            <IoCloseOutline
+              className="absolute right-4 h-5 w-5 cursor-pointer max-sm:relative max-sm:ml-[22px]"
+              onClick={handleClick}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
